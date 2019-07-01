9594c0246aa42ccdc989e0cf4e5bf121eb39eaa2
Noah Olatoye <noaholatoye@gmail.com>
2019-07-01 15:42:28 +0100

    no result message

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ListPhotos from "./ListPhotos";
+import NoPhotos from "./NoPhotos";
 const PhotoContainer = props => {
 	// Import the data fetched
 	const results = props.data;
-	let photos = results.map(pic => (
-		<ListPhotos
-			title={pic.title}
-			key={pic.id}
-			url={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${
-				pic.secret
-			}_m.jpg`}
-		/>
-	));
+	let photos;
+	if (results.length > 0) {
+		photos = results.map(pic => (
+			<ListPhotos
+				title={pic.title}
+				key={pic.id}
+				url={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${
+					pic.id
+				}_${pic.secret}_m.jpg`}
+			/>
+		));
+	} else {
+		photos = <NoPhotos />;
+	}
+
 	return (
 		<div className="photo-container">
 			<h2>Results</h2>
